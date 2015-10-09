58234f02bb31c3e1c01bea2754b2cdcac0f63b2a
Anton Zaytsev <me@antonzaytsev.com>
2015-10-09 14:17:26 +0600

    added specs for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,10 @@
 class User < ActiveRecord::Base
-  # # Include default devise modules. Others available are:
-  # # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-  # include DeviseTokenAuth::Concerns::User
-
-  # has_many :trips
-
-  # before_save :ensure_api_key
-  # has_many :api_keys
 
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /@/ }, uniqueness: { case_sensitive: false }
+
+  has_many :trips
 
   before_create :generate_authentication_token
 
@@ -22,9 +14,5 @@ class User < ActiveRecord::Base
       break unless User.find_by(authentication_token: authentication_token)
     end
   end
-
-  # def find_api_key
-  #   self.api_keys.active.first_or_create
-  # end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
+  describe 'on user create' do
+    it 'should generate token on user create' do
+      user = FactoryGirl.create :user
+      expect(user.authentication_token).to_not be_nil
+    end
+
+    it 'should generate hashed password' do
+      user = FactoryGirl.create :user
+      expect(user.password_digest).to_not be_nil
+    end
+  end
+
 end
